1a2531a974e27eaa60a7ad3ef1431320b5a634a8
fjella <fjellathecat@protonmail.com>
2025-05-19 22:38:41 +0200

    Initial Commit

---
+++ php-files/index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fjella's Cozy Corner</title>
+</head>
+<body>
+    <h1 style="background-image: url('../images/Flowers-Pixel.png');"><i>Fjella's Cozy Corner</i></h1>
+    <p><b>Welcome to my personal space on the internet!</b></p>
+    <h2>...................................................................................................................................................................................................................</h2>
+    <h2><i>What is this site?</i></h2>
+    <p>This is my personal website, where I experiment with fun and cringe stuff >w<</p>
+    <h2><i>What can you do here?</i></h2>
+    <p>You can do all sorts of random stuff (soon&trade;)</p>
+    <p>See my random fun projects (soon&trade;)</p>
+    <p>Having fun (hopefully)</p>
+</body>
+<style>
+    body {
+        background-color: pink;
+        text-align: center;
+    }
+    h1, h2, p {
+        color: deeppink;
+    }
+</style>
+</html>
